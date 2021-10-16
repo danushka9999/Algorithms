1c76bc63f4f836f81233f4e1f1e8571278b57c63
Danushka Ranasinghe <78586399+danushka9999@users.noreply.github.com>
2021-10-16 16:00:27 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Main {
+  public static void main(String[] args) {
+
+    int[] arr = {4,3,2,1};
+
+    for(int j = 1; j<arr.length;j++){
+      int key = arr[j];
+      int i = j -1;
+      while(i > 0 && arr[i] > key){
+        arr[i+1] = arr[i];
+        i = i-1;
+      }
+      arr[i+1] = key;
+    }
+  System.out.println(Arrays.toString(arr));
+  }
+}
